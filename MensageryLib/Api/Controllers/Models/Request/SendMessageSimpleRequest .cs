fdb7338300c1a6f7429ad8d38963385e65f72354
Diogo Feld <cfeld.diogo@gmail.com>
2025-05-03 10:08:37 -0300

    send simple message

---
+++ MensageryLib/Api/Controllers/Models/Request/SendMessageSimpleRequest .cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Controllers.Models.Request
+{
+    public class SendMessageSimpleRequest<T>
+    {
+        public T message { get; set; }
+        public string queue { get; set; }
+    }
+}
